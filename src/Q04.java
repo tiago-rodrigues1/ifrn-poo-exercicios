ceae4ceaf7c45989248ff6bb5b43b8685093531f
tiago-rodrigues1 <tiagords127@gmail.com>
2021-11-18 21:25:59 -0300

    add questao 4

---
+++ src/Q04.java
@@ -0,0 +1,36 @@
+/*
+* Autor: Tiago Rodrigues dos Santos
+* Data: 21/11/2021
+* Descrição: Calcula dinheiro gasto por um fumante com dados informados pelo usuário
+*/
+
+import java.util.Scanner;
+
+public class Q04 {
+
+	public static void main(String[] args) {
+		Scanner scanTeclado = new Scanner(System.in);
+		
+		System.out.print("- Digite o numero de anos de fumo: ");
+		int anosDeFumo = scanTeclado.nextInt();
+		
+		System.out.print("- Digite o numero de cigarros fumados por dia: ");
+		int cigarrosPorDia = scanTeclado.nextInt();
+		
+		System.out.print("- Digite o preco da carteira com 20 cigarros: ");
+		double precoCarteiraCigarros = scanTeclado.nextDouble();
+		
+		double precoCigarro = precoCarteiraCigarros / 20;
+		
+		int diasDeFumo = anosDeFumo * 365;
+		int quantidadeTotalCigarros = cigarrosPorDia * diasDeFumo;
+		
+		double dinheiroGasto = quantidadeTotalCigarros * precoCigarro;
+		
+		System.out.print("/n");
+		
+		System.out.printf("> Esse fumante gastou R$ %.2f com cigarros", dinheiroGasto);
+
+	}
+
+}
