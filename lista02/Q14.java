56265aa4e6d128b898f26c55f45a16e8d86438da
tiago-rodrigues1 <tiagords127@gmail.com>
2021-11-26 16:07:48 -0300

    feat: add Q14

---
+++ lista02/Q14.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Q14 {
+
+	public static void main(String[] args) {
+		Scanner scanTeclado = new Scanner(System.in);
+		
+		String campus;
+		
+		System.out.println("Código");
+		System.out.println("\n* SC \n* NC \n* CNAT \n* CANG \n* JC");
+		
+		System.out.print("\n- Digite o código do campus: ");
+		String campusCodigo = scanTeclado.next().toUpperCase();
+		
+		switch (campusCodigo) {
+			case "SC": {
+				campus = "Santa Cruz";
+				
+				break;
+			}
+				
+			case "NC": {
+				campus = "Nova Cruz";
+				
+				break;
+			}
+			
+			case "CNAT": {
+				campus = "Natal Central";
+				
+				break;
+			}
+			
+			case "CANG": {
+				campus = "Canguaretama";
+				
+				break;
+			}
+			
+			case "JC": {
+				campus = "João Câmara";
+				
+				break;
+			}
+			
+			default: {
+				campus = "Campus inválido";
+				
+				break;
+			}
+		}
+		
+		System.out.format("\n> %s: IFRN/%s", campusCodigo, campus);
+
+	}
+
+}
